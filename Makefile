0ebd0bc92bfc0ac49600c3b54afd6cc1af2b1c3b
bkolonics <placeholder@minet.net>
2023-05-04 18:43:07 +0200

    Makefile added stages

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 
+default: build createdb run
 
+run:
+	@echo "Running final.py"
+	python3 final.py
 
 
 createdb:
@@ -10,3 +14,15 @@ createdb:
 test:
 	@echo "Running tests..."
 	python3 -m unittest discover -v
+
+clean:
+	@echo "Cleaning up"
+	rm -f ecsel_database.db
+	rm -rf venv
+
+build:
+	@echo "Creating venv"
+	test -d venv || python3 -m venv venv
+	. venv/bin/activate
+	@echo "Installing requirements..."
+	pip3 install -r requirements.txt
---
+++ final.py
@@ -0,0 +1,12 @@
+import createdb
+
+def validate_country_acronym(aconym: str) -> str:
+    """function valideates country acronym"""
+    if len(aconym) != 2:
+        raise ValueError("Country acronym must be 2 characters long")
+
+    if aconym not in createdb.excel_to_dataframe('assets/countries.xlsx',
+                                        sheet_name='Countries')["Acronym"].values:
+        raise ValueError("Country acronym not in list of countries")
+
+    return aconym
