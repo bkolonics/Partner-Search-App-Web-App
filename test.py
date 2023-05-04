581c7a8c00c6bdb30b74483cd452d6c08e97732e
bkolonics <placeholder@minet.net>
2023-05-04 12:47:05 +0200

    lint

--- test.py
+++ test.py
@@ -13,6 +13,9 @@ class TestCreateDB(unittest.TestCase):
     """
 
     def test_type_excel_to_dataframe(self):
+        """
+        Test the type of the output of the function excel_to_dataframe
+        """
         self.assertEqual(type(createdb.excel_to_dataframe('assets/projects.xlsx',
                         sheet_name='Sheet1')), type(pd.DataFrame()))
         self.assertEqual(type(createdb.excel_to_dataframe('assets/participants.xlsx',
@@ -21,12 +24,19 @@ class TestCreateDB(unittest.TestCase):
                         sheet_name='Countries')), type(pd.DataFrame()))
 
     def test_content_excel_to_dataframe(self):
+        """
+        Test the content of the output of the function excel_to_dataframe
+        """
         test_df = pd.DataFrame({'A': ['D'], 'B': ['E'], 'C': ['F']})
         excel_to_dataframe = createdb.excel_to_dataframe('test_assets/test_excel.xlsx',
                         sheet_name='Feuil1')
         self.assertTrue(excel_to_dataframe.equals(test_df))
 
     def test_dataframe_to_sql(self):
+        """
+        Test the content of the output of the function dataframe_to_sql
+        Not yet implemented
+        """
         pass
 
 if __name__ == '__main__':
