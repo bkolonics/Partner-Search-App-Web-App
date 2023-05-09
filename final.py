e498f457918af2a200e9365e0e086afc11798c92
bkolonics <placeholder@minet.net>
2023-05-09 10:53:14 +0200

    Added generate dataframe function and visualisation for it

--- final.py
+++ final.py
@@ -38,9 +38,21 @@ def country_anagram_to_full_name(anagram: str) -> str:
     conn.close()
     return full_name[0]
 
+def generate_dataframe(country: str) -> list:
+    """function generates dataframe"""
+    conn = sqlite3.connect('ecsel_database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT shortName, name, activityType, organizationURL, SUM(ecContribution) FROM participants WHERE country = ? GROUP BY shortName", (country,))
+    df = cursor.fetchall()
+    conn.close()
+    return df
 
 if __name__ == '__main__':
+    st.set_page_config(page_title="Partner Search App", page_icon="assets/logo-ecsel.png")
+    st.image("assets/kdtju.png")
     st.title("Partner Search App")
     st.write("Antoine Colinet & Bence Kolonics")
-    st.selectbox("Choose a country :", map(country_anagram_to_full_name,
-                                           extract_countries_from_db()))
+    selected_country = st.selectbox("Choose a country :", extract_countries_from_db(), format_func=country_anagram_to_full_name)
+
+    st.write(f"Participants in {country_anagram_to_full_name(selected_country)}")
+    st.dataframe(generate_dataframe(selected_country), use_container_width=True)
