084a9a86cbdb6763c617675e8850ead85383a0b0
bkolonics <placeholder@minet.net>
2023-05-09 17:03:45 +0200

    added graphs

--- final.py
+++ final.py
@@ -5,6 +5,7 @@ TODO: Add more here
 import sqlite3
 import streamlit as st
 import pandas as pd
+import altair as alt
 import createdb
 
 
@@ -71,6 +72,20 @@ def generate_dataframe_project_coordinators(country: str) -> pd.DataFrame:
     conn.close()
     return df_participants
 
+def generate_dataframe_10_most_active_countries() -> pd.DataFrame:
+    """function generates dataframe for 10 most active countries"""
+    conn = sqlite3.connect('ecsel_database.db')
+    query = """SELECT country, SUM(ecContribution)
+               FROM participants
+               GROUP BY country
+               ORDER BY SUM(ecContribution) DESC
+               LIMIT 10"""
+    df_participants = pd.read_sql(query, conn)
+    df_participants = df_participants.rename(columns={'country': 'Country',
+                                                        'SUM(ecContribution)': 'Grants'})
+    conn.close()
+    return df_participants
+
 if __name__ == '__main__':
     st.set_page_config(page_title="Partner Search App", page_icon="assets/logo-ecsel.png")
     st.image("assets/kdtju.png")
@@ -81,10 +96,22 @@ if __name__ == '__main__':
 
     st.subheader(f"Participants in {country_anagram_to_full_name(selected_country)}")
     st.dataframe(generate_dataframe(selected_country), use_container_width=True)
+
+    st.subheader(f"Top 10 Participants in {country_anagram_to_full_name(selected_country)}")
+    chart = alt.Chart(generate_dataframe(selected_country).head(10)).mark_bar().encode(
+        x='Short Name',
+        y='Grants',
+        color='Activity Type',
+        tooltip=['Short Name', 'Grants', 'Activity Type', 'Organization URL']
+    ).interactive()
+    st.altair_chart(chart, use_container_width=True)
+
     st.download_button(label="Download Participants as CSV",
                           data=generate_dataframe(selected_country).to_csv(),
                             file_name=f"{country_anagram_to_full_name(selected_country)}.csv",
                             mime="text/csv")
+
+    st.divider()
 
     st.subheader(f"Project Coordinators in {country_anagram_to_full_name(selected_country)}")
     st.dataframe(generate_dataframe_project_coordinators(selected_country),
@@ -92,4 +119,21 @@ if __name__ == '__main__':
     st.download_button(label="Download Project Coordinators as CSV",
                             data=generate_dataframe_project_coordinators(selected_country).to_csv(),
                             file_name=f"{country_anagram_to_full_name(selected_country)}_coordinators.csv", # pylint: disable=line-too-long
+                            mime="text/csv")
+
+    st.divider()
+
+    st.subheader("Top 10 Countries")
+    st.dataframe(generate_dataframe_10_most_active_countries(), use_container_width=True)
+    chart = alt.Chart(generate_dataframe_10_most_active_countries()).mark_bar().encode(
+        x='Country',
+        y='Grants',
+        tooltip=['Country', 'Grants']
+    ).interactive()
+
+    st.altair_chart(chart, use_container_width=True)
+
+    st.download_button(label="Download Top 10 Countries as CSV",
+                            data=generate_dataframe_10_most_active_countries().to_csv(),
+                            file_name="top_10_countries.csv",
                             mime="text/csv")
